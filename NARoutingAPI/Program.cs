f5690c26d124b0a586a8cdb1614801ac0376371a
Anjali Marella <anj.marella@gmail.com>
2026-09-18 10:55:40 -0500

    compiled main functionality into Program.cs

--- NARoutingAPI/Program.cs
+++ NARoutingAPI/Program.cs
@@ -9,8 +9,30 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-// redirects HTTP requests to HTTPS (adds securityd)
+// redirects HTTP requests to HTTPS (adds security)
 app.UseHttpsRedirection();
 
+var routeHandler = new RoutingLogic();
+
+app.MapGet("/{countryCode}", (string countryCode) =>
+{
+    // changes input into all uppercase
+    var destination = countryCode.ToUpperInvariant();
+
+    // validate country code is in Map
+    if (!routeHandler.IsValidCountry(destination))
+    {
+        return Results.BadRequest(new { error = "Invalid country code." });
+    }
+
+    // find shortest route
+    var route = routeHandler.BFSRoute(destination);
+
+    return Results.Ok(new
+    {
+        destination,
+        list = route
+    });
+});
 
 app.Run();
