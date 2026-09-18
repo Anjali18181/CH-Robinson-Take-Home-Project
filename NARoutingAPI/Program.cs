cc6ff5d6279bed2c0730e3a34753ed0124a9e460
Anjali Marella <anj.marella@gmail.com>
2026-09-18 10:41:36 -0500

    removed template code

--- NARoutingAPI/Program.cs
+++ NARoutingAPI/Program.cs
@@ -1,41 +1,16 @@
+// creates WebApplicationBuilder object and any necessary configurations
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+// builds WebApplication object
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+// Opens endpoint to OpenAPI (documentation for API)
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+// redirects HTTP requests to HTTPS (adds securityd)
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- NARoutingAPI/obj/Debug/net10.0/NARoutingAPI.AssemblyInfo.cs
+++ NARoutingAPI/obj/Debug/net10.0/NARoutingAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NARoutingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b939befb5b742b43cbae0d4988fdfadf9469e7f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03588a62dfe0a0673b19924b205810f25a395ff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NARoutingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NARoutingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
